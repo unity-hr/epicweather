93136d70e6e28faf95b1d77f7d3312a42a310d58
Matija Marohnić <matija.marohnic@gmail.com>
2015-06-09 23:38:33 +0200

    Crop carousel images on smaller sizes

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -1,12 +1,9 @@
 import Flickity from 'flickity';
-import imagesLoaded from 'imagesloaded';
 
 var container = document.querySelector('.section-epicweather .section-items');
 
-imagesLoaded(container, function () {
-  new Flickity(container, {
-    prevNextButtons: false,
-    wrapAround: true,
-    autoPlay: true
-  });
+new Flickity(container, {
+  prevNextButtons: false,
+  wrapAround: true,
+  autoPlay: true
 });
